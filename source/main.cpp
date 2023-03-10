95c65b43d67fd9342d2494002cc628eed1b48b76
tstumpman <tstumpman@gmail.com>
2023-03-10 17:58:17 -0500

    init commit

---
+++ source/main.cpp
@@ -0,0 +1,16 @@
+
+//#include "Game/game.h"
+#include <iostream>
+int main(int argc, char** argv) {
+
+	//Game game;
+	//bool success = game.initialize();
+
+	//if (success) {
+	//	game.runLoop();
+	//}
+	//game.shutdown();
+
+	std::cout << "Hello, World!";
+	return 0;
+}
