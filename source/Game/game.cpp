47141b87ab4099cf618b991cd0de4f1996f8907a
tstumpman <tstumpman@gmail.com>
2023-03-10 21:04:15 -0500

    A blank window now appears.  First success!

--- source/Game/game.cpp
+++ source/Game/game.cpp
@@ -4,16 +4,45 @@
 Game::Game() {
 	isQuitting = false;
 	isRunning = true;
-
+	mWindow = nullptr;
 }
 
 bool Game::initialize() {
-	int result = SDL_Init(SDL_INIT_VIDEO);
+	int sdlFlags = SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER;
+	int sdlResult = SDL_Init(sdlFlags);
 
-	if (result == 0) {
-		return true;
+	if (sdlResult != 0) {
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return false;
 	}
-	return false;
+
+	int sdlWindowOptions = SDL_WINDOW_OPENGL;
+	mWindow = SDL_CreateWindow(
+	"Window Title Goes Here",//Window Title
+		100,//Top Left X coord
+	    100,//Top Left Y coord
+		240,//Window Width
+		160,//Window Height
+		sdlWindowOptions// Window flags
+	);
+
+	if (!mWindow) {
+		logSdlError(SDL_GetError());
+		return false;
+	}
+	return true;
+}
+
+void Game::logSdlError(std::string errorMessage) {
+	std::vector<std::string> a = std::vector<std::string>();
+	a.push_back(errorMessage);
+	logSdlError(a);
+}
+
+void Game::logSdlError(std::vector<std::string> messages) {
+	for (int i = 0; i < messages.size(); i++) {
+		SDL_Log(messages[i].c_str());
+	}
 }
 
 void Game::runLoop() {
@@ -27,10 +56,11 @@ void Game::runLoop() {
 }
 
 void Game::shutdown() {
+	SDL_DestroyWindow(mWindow);
+	SDL_Quit();
 }
 
 void Game::processInput() {
-
 }
 
 void Game::updateGame() {
--- source/Game/game.h
+++ source/Game/game.h
@@ -1,26 +1,27 @@
 #ifndef CLASS_GAME
 #define CLASS_GAME
 
-
+#include <string>
+#include <vector>
 struct SDL_Window;
 class Game {
-public:
+public://Functions
 	Game();
 	bool initialize();
-
 	void runLoop();
-
 	void shutdown();
-private:
 
+private://Functions
 	void processInput();
 	void updateGame();
 	void generateOutput();
 	void renderAudio();
 	void renderGraphics();
+	void logSdlError( std::string errorMessage);
+	void logSdlError(std::vector<std::string> messages);
 
-	//SDL_Window* mWindow;
-
+private://Data
+	SDL_Window* mWindow;
 	bool isRunning;
 	bool isQuitting;
 protected:
