b798428728e89246165e98498ce8c408f0e3cfbc
tstumpman <tstumpman@gmail.com>
2023-03-10 19:23:12 -0500

    Main Game Loop up and running.

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,15 @@
-
-//#include "Game/game.h"
+#include "Game/game.h"
 #include <iostream>
+
 int main(int argc, char** argv) {
 
-	//Game game;
-	//bool success = game.initialize();
+	Game game;
+	bool success = game.initialize();
 
-	//if (success) {
-	//	game.runLoop();
-	//}
-	//game.shutdown();
+	if (success) {
+		game.runLoop();
+	}
+	game.shutdown();
 
-	std::cout << "Hello, World!";
 	return 0;
 }
