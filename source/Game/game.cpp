7ad41ea7b11c02d061dd47896be192217f48eabc
tstumpman <tstumpman@gmail.com>
2023-03-13 16:25:05 -0400

    Now using the delta time in the game update.

--- source/Game/game.cpp
+++ source/Game/game.cpp
@@ -69,7 +69,7 @@ void Game::runLoop() {
 	while (isRunning) {
 		deltaTime = getDeltaTime(previousTime, maxDelta);
 		processInput();
-		updateGame();
+		updateGame(deltaTime);
 		generateOutput();
 	}
 }
